4205904d97d95730e291728e94322cf441806da3
dean <iamdekanos@gmail.com>
2021-05-11 17:20:07 +0300

    simple blog with simple crud ops

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+const dotenv = require("dotenv");
+const DB_CONNECT = require("./config/db");
+
+// load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// express app
+const app = express();
+
+// connect to mongodb
+DB_CONNECT();
+
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware & static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+// blog routes
+app.use("/blogs", blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
+
+// Init server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
